9116344101bc5d2354839396461759d33cab9fa7
Am-miid <127655149+Am-miid@users.noreply.github.com>
2023-11-06 12:04:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const autoCompleteConfig = {
             return movie.Title;
     },
     async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com', {
+        const response = await axios.get('https://www.omdbapi.com', {
             params: {
                 apikey: '1b231c52',
                 s: searchTerm
